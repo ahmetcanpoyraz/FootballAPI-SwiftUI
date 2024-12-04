4efb92d314310127565c0ff1d24e7cba5a63dca7
ahmetcanpoyraz <ahmetcan.poyraz@izmirteknoloji.com.tr>
2024-12-04 16:24:50 +0300

    api bağlantısı gerçekletirildi

---
+++ FootballAPI-SwiftUI/Models/LeagueModel.swift
@@ -0,0 +1,8 @@
+//
+//  LeagueModel.swift
+//  FootballAPI-SwiftUI
+//
+//  Created by İzmir İnovasyon ve Teknoloji on 4.12.2024.
+//
+
+import Foundation
---
+++ FootballAPI-SwiftUI/Services/APIClient.swift
@@ -0,0 +1,8 @@
+//
+//  APIClient.swift
+//  FootballAPI-SwiftUI
+//
+//  Created by İzmir İnovasyon ve Teknoloji on 3.12.2024.
+//
+
+import Foundation
